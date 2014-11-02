7b05fa48fb38f626985326b7792235c5c4869dbb
XandoBit <x-ando@hotmail.com>
2014-11-02 17:10:32 +0000

    gemas chartkick

--- model.rb
+++ model.rb
@@ -6,15 +6,36 @@ class ShortenedUrl
    property :url_opc, Text
    property :usuario, Text
  
+   has n, :visits
 end
 
-class ShortenedUrl
+class Visit
   include DataMapper::Resource
 
-  property :id, Serial
-  property :short, Text
-  property :url, Text
+  property  :id,          Serial
+  property  :created_at,  DateTime
+  property  :ip,          IPAddress
+  property  :country,     String
+  belongs_to  :link
 
-  has n, :visits
+  after :create, :set_country
+
+  def set_country
+    xml = RestClient.get "http://api.hostip.info/get_xml.php?ip=#{ip}"··
+    self.country = XmlSimple.xml_in(xml.to_s, ...
+    self.save
+  end
+  
+  def get_remote_ip(env)
+  puts "request.url = #{request.url}"
+  puts "request.ip = #{request.ip}"
+  if addr = env['HTTP_X_FORWARDED_FOR']
+    puts "env['HTTP_X_FORWARDED_FOR'] = #{addr}"
+    addr.split(',').first.strip
+  else
+    puts "env['REMOTE_ADDR'] = #{env['REMOTE_ADDR']}"
+    env['REMOTE_ADDR']
+  end
 end
-
+  
+end
