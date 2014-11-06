14505eb39f9efcf4220d37a1d28026b0ab5f4c6e
Rushil Lakhani <rushil@rushil-VirtualBox.(none)>
2014-11-06 19:14:00 +0000

    Modificando modelo

--- model.rb
+++ model.rb
@@ -5,37 +5,32 @@ class ShortenedUrl
    property :url, Text
    property :url_opc, Text
    property :usuario, Text
+   property :email, Text
+   property :created_at, DataTime
+   property :n_visits
  
    has n, :visits
 end
+
 
 class Visit
   include DataMapper::Resource
 
   property  :id,          Serial
-  property  :created_at,  DateTime
   property  :ip,          IPAddress
+  property  :created_at,  DateTime
   property  :country,     String
-  belongs_to  :link
+  #belongs_to  :link
 
-  after :create, :set_country
+  belongs_to  :shortened_url
+
+  before :create, :set_country
+end
+
 
   def set_country
-    xml = RestClient.get "http://api.hostip.info/get_xml.php?ip=#{ip}"··
-    self.country = XmlSimple.xml_in(xml.to_s, ...
+    xml = RestClient.get "http://api.hostip.info/get_xml.php?ip=#{ip}"
+    self.country = XmlSimple.xml_in(xml.to_s, { 'ForceArray' => false })['CountryName'].to_s
     self.save
   end
-  
-  def get_remote_ip(env)
-  puts "request.url = #{request.url}"
-  puts "request.ip = #{request.ip}"
-  if addr = env['HTTP_X_FORWARDED_FOR']
-    puts "env['HTTP_X_FORWARDED_FOR'] = #{addr}"
-    addr.split(',').first.strip
-  else
-    puts "env['REMOTE_ADDR'] = #{env['REMOTE_ADDR']}"
-    env['REMOTE_ADDR']
-  end
-end
-  
-end
+#end
